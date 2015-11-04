6b12eff3e1c91fb5d383596f15e3a1692ffa5815
Andrew Scagnelli <andrew.scagnelli@gmail.com>
2015-11-04 13:38:15 -0500

    FINDING-7215 Adding Mocha before/after support
    Mocha uses `before` and `after` in place of Jasmine's `beforeAll` and
    `afterAll`.

--- jasmine.js
+++ jasmine.js
@@ -6,13 +6,16 @@ module.exports = {
     ],
     "env": {
         "jasmine": true,
-        "phantomjs": true
+        "phantomjs": true,
+        "mocha": true
     },
     "globals": {
         "loadFixtures": false,
         "appendLoadFixtures": false,
         "setFixtures": false,
-        "sandbox": false
+        "sandbox": false,
+        "before": false,
+        "after": false
     },
     "rules": {
         "jasmine/no-suite-dupes": [1, "branch"],
