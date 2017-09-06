2d0e24ef13d97daffd599e4fa1d7f3034122ba82
John Rodriguez <jrodriguez.0987+github@gmail.com>
2017-09-06 14:55:41 -0400

    Add rules for flow type linting (#28)
    * Add rules for flow type linting
    
    * change to warnings instead of errors

--- es6.js
+++ es6.js
@@ -2,8 +2,7 @@
 
 module.exports = {
     "plugins": [
-        "flowtype",
-        "flow-vars"
+        "flowtype"
     ],
     "parser": "babel-eslint",
     "parserOptions": {
@@ -14,8 +13,15 @@ module.exports = {
     },
     "rules": {
         "strict": 0,
-        "flow-vars/define-flow-type": 1,
-        "flow-vars/use-flow-type": 1,
+        "flowtype/define-flow-type": 1,
+        "flowtype/use-flow-type": 1,
+        "flowtype/boolean-style": [1, "boolean"],
+        "flowtype/no-dupe-keys": 1,
+        "flowtype/no-primitive-constructor-types": 1,
+        "flowtype/no-types-missing-file-annotation": 1,
+        "flowtype/require-valid-file-annotation": [1, "never"],
+        "flowtype/space-after-type-colon": [1, "always"],
+        "flowtype/union-intersection-spacing": [1, "always"],
         "constructor-super": 2,
         "no-const-assign": 2,
         "no-dupe-class-members": 2,
